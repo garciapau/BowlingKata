93fcc4018321c2cce9d257284da0bca4d9adb3ac
Pau Garcia <pau.garcia@clarivate.com>
2018-10-28 15:28:16 +0100

    refactor tests

--- src/test/java/pau/garcia/kata/BowlingGameTest.java
+++ src/test/java/pau/garcia/kata/BowlingGameTest.java
@@ -1,29 +1,44 @@
 package pau.garcia.kata;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class BowlingGameTest {
+    private BowlingGame bowlingGame;
+
+    @Before
+    public void setUp() throws Exception {
+        bowlingGame = new BowlingGame();
+    }
+
     @Test
-    public void testWhenNoScoresReturnsZero() {
-        BowlingGame bowlingGame = new BowlingGame();
+    public void whenNoScoresReturnsZero() {
         Assert.assertEquals(0, bowlingGame.score());
     }
     @Test
-    public void testWhenAllZerosReturnsZero() {
-        BowlingGame bowlingGame = new BowlingGame();
-        for (int i = 0; i < 20; i++) {
-            bowlingGame.roll(0);
-        }
+    public void whenAllZerosReturnsZero() {
+        annotateRolls(20, 0);
         Assert.assertEquals(0, bowlingGame.score());
     }
 
     @Test
-    public void testWhenAllFiveReturns100() {
-        BowlingGame bowlingGame = new BowlingGame();
-        for (int i = 0; i < 20; i++) {
-            bowlingGame.roll(5);
-        }
+    public void whenAllFiveReturns100() {
+        annotateRolls(20, 5);
         Assert.assertEquals(100, bowlingGame.score());
+    }
+
+    @Test
+    public void whenOneSpareAndRestZeroShouldReturn10() {
+        bowlingGame.roll(10);
+        annotateRolls(19, 0);
+        Assert.assertEquals(10, bowlingGame.score());
+    }
+
+
+    private void annotateRolls(int times, int pinsDown) {
+        for (int i = 0; i < times; i++) {
+            bowlingGame.roll(pinsDown);
+        }
     }
 }
