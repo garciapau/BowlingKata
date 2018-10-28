ea48c1b6172b606c1d9591394d3dd6cbff0bb73f
Pau Garcia <pau.garcia@clarivate.com>
2018-10-28 15:13:39 +0100

    create test scoring all zeros

--- src/main/java/pau/garcia/kata/BowlingGame.java
+++ src/main/java/pau/garcia/kata/BowlingGame.java
@@ -1,7 +1,13 @@
 package pau.garcia.kata;
 
 public class BowlingGame {
+    private int score;
+
     public int score() {
         return 0;
+    }
+
+    public void roll(int pinsDown) {
+        score += pinsDown;
     }
 }
--- src/test/java/pau/garcia/kata/BowlingGameTest.java
+++ src/test/java/pau/garcia/kata/BowlingGameTest.java
@@ -5,8 +5,17 @@ import org.junit.Test;
 
 public class BowlingGameTest {
     @Test
-    public void testNoScoresReturnsZero() {
+    public void testWhenNoScoresReturnsZero() {
         BowlingGame bowlingGame = new BowlingGame();
         Assert.assertEquals(0, bowlingGame.score());
     }
+    @Test
+    public void testWhenAllZerosReturnsZero() {
+        BowlingGame bowlingGame = new BowlingGame();
+        for (int i = 0; i < 20; i++) {
+            bowlingGame.roll(0);
+        }
+        Assert.assertEquals(0, bowlingGame.score());
+    }
+
 }
